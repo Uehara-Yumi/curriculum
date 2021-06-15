918d3c3e95479c29e8029566ee1eb41bfc0495ef
Uehara-Yumi <84459334+Uehara-Yumi@users.noreply.github.com>
2021-06-15 22:06:28 +0900

    課題提出（第2章チェックテスト）
    第2章チェックテストの修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -7,13 +7,13 @@ public class Check {
 	private static String firstName = "上原";
 	private static String lastName = "優海";
 
-	private static void printName() {
+	private static void printName(String firstName, String lastName){
 		System.out.println("printNameメソッド → " + firstName + lastName);
 	}
 
 
 	public static void main(String[] args) {
-		printName();
+		printName(firstName, lastName);
 
 		Pet pet = new Pet(Constants.CHECK_CLASS_HOGE, Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
